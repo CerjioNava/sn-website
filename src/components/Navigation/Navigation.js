d99c5438c666f96594b7302a734fbf3b6ecad31e
unknown <cerjionava@gmail.com>
2021-06-17 17:38:27 -0400

    Background gradient color

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,22 +6,22 @@ const Navigation = () => {
     return (
         <nav className="flex justify-between bb bw2 b--light-blue" >            
             <div>
-                <p class="f3 pa3 link bg-animate hover-bg-light-blue pointer b ml3">
+                <p class="f3 pa1 link bg-animate hover-bg-lightest-blue pointer b ml4">
                     <a class="alink">SERGIO NAVA</a>
                 </p>
             </div>
 
             <div class="flex">
-                <p className="f3 pa3 link bg-animate hover-bg-light-blue pointer">
+                <p className="f3 pa1 link bg-animate hover-bg-lightest-blue pointer mr4">
                     <a href="https://www.google.com/" class="alink">Home</a>
                 </p>
-                <p className="f3 pa3 link bg-animate hover-bg-light-blue pointer">
+                <p className="f3 pa1 link bg-animate hover-bg-lightest-blue pointer mr4">
                     <a href="" class="alink">About</a>
                 </p>
-                <p className="f3 pa3 link bg-animate hover-bg-light-blue pointer">
+                <p className="f3 pa1 link bg-animate hover-bg-lightest-blue pointer mr4">
                     <a href="" class="alink">Portfolio</a>    
                 </p>
-                <p className="f3 pa3 link bg-animate hover-bg-light-blue pointer mr3">
+                <p className="f3 pa1 link bg-animate hover-bg-lightest-blue pointer mr4">
                     <a href="" class="alink">Contact</a>
                 </p>
             </div>            
